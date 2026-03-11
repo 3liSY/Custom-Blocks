3b3d1944fbe6002e1f5234e27e795f538c678025
3liSY <666640@gmail.com>
2026-03-11 07:03:11 +0400

    a

--- src/main/java/com/customblocks/block/CustomBlock.java
+++ src/main/java/com/customblocks/block/CustomBlock.java
@@ -1,6 +1,9 @@
 package com.customblocks.block;
 
 import net.minecraft.block.Block;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
 
@@ -20,5 +23,24 @@ public class CustomBlock extends Block {
 
     public String getCustomDisplayName() {
         return displayName;
+    }
+
+    public static class CustomBlockItem extends BlockItem {
+        private final String displayName;
+
+        public CustomBlockItem(CustomBlock block, Item.Settings settings) {
+            super(block, settings);
+            this.displayName = block.getCustomDisplayName();
+        }
+
+        @Override
+        public Text getName() {
+            return Text.literal(displayName);
+        }
+
+        @Override
+        public Text getName(ItemStack stack) {
+            return Text.literal(displayName);
+        }
     }
 }
